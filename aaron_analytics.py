c4a647cd0d3133bdb0d5699e93090c3a9af036f8
Aaron <epiihrawp@gmail.com>
2024-09-10 17:18:49 -0500

    Added aaron_analytics.py

---
+++ aaron_analytics.py
@@ -0,0 +1,10 @@
+""" This project 3 demonstates my ability to fench, parse and write differnt file types with python.
+I plan to use some market files for my sample codes to process.
+I will process .txt, .csv, .xlsx, and .json files"""
+
+# Standard library imports
+import csv
+import pathlib 
+
+# External library imports (requires virtual environment)
+import requests 
